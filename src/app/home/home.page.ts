2a39a9c5b721a6a253d9835be5075108c4f52cc5
Njabulo96 <njabulomlang96@gmail.com>
2020-03-06 16:41:37 +0200

    changes

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
-
+export class HomePage implements OnInit{
+  dbProduct = firebase.firestore().collection('Products');
+  myProduct = [];
+  val = '';
   constructor() {}
 
+  ngOnInit() {
+    this.getProductsbyCategory('Pottery')
+  }
+  getProductsbyCategory(name) {
+    this.val='active';
+    this.dbProduct.where('category','==',name).onSnapshot((res)=>{
+      this.myProduct = [];
+      res.forEach((doc)=>{
+        this.myProduct.push({data: doc.data(), id : doc.id})
+      })
+      // console.log("My items ", this.myProduct);
+      
+    })
+  }
+  viewProduct(val) {
+    
+  }
 }
