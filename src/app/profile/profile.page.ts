fe208c9a2ea4f1b9e2343d987c2ee1cb27e08442
Njabulo96 <njabulomlang96@gmail.com>
2020-03-11 08:22:15 +0200

    changes

--- src/app/profile/profile.page.ts
+++ src/app/profile/profile.page.ts
@@ -12,49 +12,50 @@ export class ProfilePage implements OnInit {
 
   uid
   Allorders = [];
-  Profile= []
+  Profile = []
   isprofile = false;
 
   profile = {
     image: '',
     name: '',
     number: '',
-    address: '',  
+    address: '',
     email: '',
     uid: '',
   }
-  admin={
-     uid: '',
-     email: ''
-    }
-  
-    orderHistory = []
-  constructor(public alertCtrl: AlertController, private router: Router, public navCtrl : NavController) {
-    if(firebase.auth().currentUser) {
+  admin = {
+    uid: '',
+    email: ''
+  }
+
+  orderHistory = []
+  constructor(public alertCtrl: AlertController, private router: Router, public navCtrl: NavController) {
+    if (firebase.auth().currentUser) {
       this.profile.email = firebase.auth().currentUser.email;
       this.uid = firebase.auth().currentUser.uid;
-     } else {
-       console.log('error user not logged in');
-       
-     }
-    
-   }
+    } else {
+      console.log('error user not logged in');
+
+    }
+
+  }
 
   ngOnInit() {
-
-
+    setTimeout(() => {
+      
     firebase.auth().onAuthStateChanged(user => {
       if (user) {
         this.admin.uid = user.uid
         this.admin.email = user.email
-      this.getProfile();
-      this.GetOrders();
-      this.getHistory();
+        this.getProfile();
+        this.GetOrders(user.uid);
+        this.getHistory();
       } else {
         console.log('no admin');
-        
       }
     })
+    }, 1000);
+
     // this.GetOrders();
   }
   logout() {
@@ -63,35 +64,35 @@ export class ProfilePage implements OnInit {
   popBack() {
     this.navCtrl.pop();
   }
-  getProfile(){
+  getProfile() {
     firebase.firestore().collection('UserProfile').where('uid', '==', this.admin.uid).onSnapshot(snapshot => {
-      this.Profile=[]
+      this.Profile = []
       if (snapshot.empty) {
         this.isprofile = false;
       } else {
         this.isprofile = true;
         snapshot.forEach(doc => {
-          this.profile.image=doc.data().image;
-          this.profile.name=doc.data().name;
-          this.profile.number=doc.data().number;
-          this.profile.address=doc.data().address;
-          this.profile.email=doc.data().email;
+          this.profile.image = doc.data().image;
+          this.profile.name = doc.data().name;
+          this.profile.number = doc.data().number;
+          this.profile.address = doc.data().address;
+          this.profile.email = doc.data().email;
         })
       }
     })
   }
 
-  editProfile(){
+  editProfile() {
     console.log("Going to edit profile");
-    
+
     this.router.navigateByUrl('/edit-profile')
   }
-  ordersOpen:boolean = false;
-  historyOpen:boolean = false;
-  toggleOrders(){
+  ordersOpen: boolean = false;
+  historyOpen: boolean = false;
+  toggleOrders() {
     var ordersArrow = document.getElementById("orders-arrow");
     var myOrders = document.getElementById("myOrders");
-    if(this.ordersOpen == false){
+    if (this.ordersOpen == false) {
       this.ordersOpen = true;
       ordersArrow.style.transform = "rotateX(180DEG)";
       myOrders.style.height = "auto";
@@ -100,7 +101,7 @@ export class ProfilePage implements OnInit {
       this.historyOpen = true;
       this.toggleHistory()
     }
-    else{
+    else {
       this.ordersOpen = false;
       ordersArrow.style.transform = "rotateX(0DEG)";
       myOrders.style.height = "0";
@@ -108,10 +109,10 @@ export class ProfilePage implements OnInit {
       myOrders.style.padding = "0px";
     }
     // console.log("orders: " + this.ordersOpen);
-    
+
   }
 
-  viewOrder(item){
+  viewOrder(item) {
     console.log("GGH ", item);
 
     let navigationExtras: NavigationExtras = {
@@ -130,16 +131,16 @@ export class ProfilePage implements OnInit {
     //   }
     // };
     this.navCtrl.navigateForward(['/order-tracking'], navigationExtras);
-  
-    
+
+
     // this.router.navigateByUrl('/order-tracking')
   }
 
-  toggleHistory(){
-    
+  toggleHistory() {
+
     var myArrow = document.getElementById("history-arrow");
     var myHistory = document.getElementById("myHistory");
-    if(this.historyOpen == false){
+    if (this.historyOpen == false) {
       this.historyOpen = true;
       myArrow.style.transform = "rotateX(180DEG)";
       myHistory.style.height = "auto";
@@ -148,25 +149,24 @@ export class ProfilePage implements OnInit {
       this.ordersOpen = true;
       this.toggleOrders()
     }
-    else{
+    else {
       this.historyOpen = false;
       myArrow.style.transform = "rotateX(0DEG)";
       myHistory.style.height = "0";
       myHistory.style.opacity = "0";
       myHistory.style.padding = "0px";
-      
+
     }
     // console.log("history: " +this.historyOpen); 
   }
 
-  GetOrders() {
-    firebase.firestore().collection("Order").where('userID', '==', firebase.auth().currentUser.uid).onSnapshot((data) => {
+  GetOrders(uid) {
+    firebase.firestore().collection("Order").where('userID', '==', uid).onSnapshot((data) => {
       this.Allorders = [];
       data.forEach((item) => {
-        this.Allorders.push({ ref: item.id, info: item.data()})
+        this.Allorders.push({ ref: item.id, info: item.data() })
+        console.log(this.Allorders);
       })
-      //  console.log("orders ", this.Allorders);
-      
     })
   }
   viewReciept(id) {
@@ -176,13 +176,13 @@ export class ProfilePage implements OnInit {
     firebase.firestore().collection("orderHistory").where('userID', '==', firebase.auth().currentUser.uid).onSnapshot((data) => {
       this.orderHistory = [];
       data.forEach((item) => {
-        this.orderHistory.push({ ref: item.id, info: item.data()})
+        this.orderHistory.push({ ref: item.id, info: item.data() })
       })
       //  console.log("orders ", this.Allorders);
-      
+
     })
   }
-  trackOrder(id){
+  trackOrder(id) {
     let navigationExtras: NavigationExtras = {
       queryParams: {
         id: id,
