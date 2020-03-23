7943389e09c087dee00e9f5d09255c15d352bb20
Njabulo96 <njabulomlang96@gmail.com>
2020-03-23 14:02:16 +0200

    fgdf

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -41,14 +41,18 @@ var firebaseConfig = {
   // messagingSenderId: "728167140242",
   // appId: "1:728167140242:web:7b16d2f988fee781c90f4b",
   // measurementId: "G-TKFFCKLRTQ"
-  // apiKey: "AIzaSyD96pHc4rjR85yGc7y1Es4e1mGocFqHVjE",
-  // authDomain: "pitseng-arts.firebaseapp.com",
-  // databaseURL: "https://pitseng-arts.firebaseio.com",
-  // projectId: "pitseng-arts",
-  // storageBucket: "pitseng-arts.appspot.com",
-  // messagingSenderId: "628652538196",
-  // appId: "1:628652538196:web:b17d822fe6eb6702f9f0c1",
-  // measurementId: "G-N7X15WH9QB"
+
+
+  apiKey: "AIzaSyD96pHc4rjR85yGc7y1Es4e1mGocFqHVjE",
+  authDomain: "pitseng-arts.firebaseapp.com",
+  databaseURL: "https://pitseng-arts.firebaseio.com",
+  projectId: "pitseng-arts",
+  storageBucket: "pitseng-arts.appspot.com",
+  messagingSenderId: "628652538196",
+  appId: "1:628652538196:web:b17d822fe6eb6702f9f0c1",
+  measurementId: "G-N7X15WH9QB"
+
+
   // apiKey: "AIzaSyCnRo3S9hOi1R9vMpnYNW5ajcpJh5G1tvo",
   // authDomain: "pitsengtest.firebaseapp.com",
   // databaseURL: "https://pitsengtest.firebaseio.com",
@@ -57,14 +61,16 @@ var firebaseConfig = {
   // messagingSenderId: "728167140242",
   // appId: "1:728167140242:web:7b16d2f988fee781c90f4b",
   // measurementId: "G-TKFFCKLRTQ"
-  apiKey: "AIzaSyAxqR5pSWPudGtn3cvZlubPjwY-U6K5NRc",
-  authDomain: "pitsengdb-e8f33.firebaseapp.com",
-  databaseURL: "https://pitsengdb-e8f33.firebaseio.com",
-  projectId: "pitsengdb-e8f33",
-  storageBucket: "pitsengdb-e8f33.appspot.com",
-  messagingSenderId: "262790398266",
-  appId: "1:262790398266:web:eeeebec9076853a0cc1003",
-  measurementId: "G-ZJEW6XRQ6M"
+
+
+  // apiKey: "AIzaSyAxqR5pSWPudGtn3cvZlubPjwY-U6K5NRc",
+  // authDomain: "pitsengdb-e8f33.firebaseapp.com",
+  // databaseURL: "https://pitsengdb-e8f33.firebaseio.com",
+  // projectId: "pitsengdb-e8f33",
+  // storageBucket: "pitsengdb-e8f33.appspot.com",
+  // messagingSenderId: "262790398266",
+  // appId: "1:262790398266:web:eeeebec9076853a0cc1003",
+  // measurementId: "G-ZJEW6XRQ6M"
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
